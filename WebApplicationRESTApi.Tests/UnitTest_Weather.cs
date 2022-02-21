146ce21074fda45b7b0f58dc006df515ecf3c874
fred <domaine75@gmail.com>
2022-02-21 10:04:35 +0100

    Add unit test project

---
+++ WebApplicationRESTApi.Tests/UnitTest_Weather.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplicationRESTApi.Tests
+{
+  [TestClass]
+  public class UnitTest_Weather
+  {
+    [TestMethod]
+    public void TestMethod_1()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
