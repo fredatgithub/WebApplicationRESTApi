4dd7c8ebf6febe4e8480282eadc64b8e868d732a
fred <domaine75@gmail.com>
2022-02-21 10:42:29 +0100

    Add unit test for web app rest api2 in srv sub-directory

---
+++ src/services/CommitAPI.Tests/WebAppRESTApi2.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebAppRESTApi2.UnitTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
